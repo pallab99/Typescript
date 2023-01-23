c16920648f5c2df32c30c940da936dc40e08d077
Pallab Majumdar <kitkatpallab@gmail.com>
2023-01-23 12:07:47 +0600

    second commit

---
+++ ts_object.ts
@@ -0,0 +1,38 @@
+let employee: object;
+
+employee = {
+    firstName: "Pallab",
+    lastName: "Majumdar",
+    passion: "software Engineer",
+    age: 25
+};
+
+console.log(employee);
+
+console.log(employee.firstName);
+
+let employee1: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+};
+
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+let employee2: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+} = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
