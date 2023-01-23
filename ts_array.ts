073ddc5163aa1ae36fcfa361692ac6734eda850f
Pallab Majumdar <kitkatpallab@gmail.com>
2023-01-23 17:25:05 +0600

    third commit

--- ts_array.ts
+++ ts_array.ts
@@ -12,5 +12,25 @@ const numbers = [1, 2, 3]; // inferred to type number[]
 numbers.push(4); // no error
 // comment line below out to see the successful assignment
 // numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
-let head: number = numbers[0]; 
+let head: number = numbers[0];
 console.log(head);// no error
+
+let skills: string[] = [];
+skills[0] = "Problem Solving";
+skills[1] = "Programming";
+
+
+skills.push("Software Engineer");
+
+console.log(skills);
+
+console.log(typeof (skills));
+
+let scores = ['Programming', 5, 'Software Design', 4];
+
+console.log(scores);
+
+let scores1: (string | number)[];
+scores1 = ['Programming', 5, 'Software Design', 4];
+
+console.log(scores1);
